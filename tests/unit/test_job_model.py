c40ca3ef152f523775ec9b71c05369f746c6d24a
Douglas Quintanilha Barbosa Ferreira | Stone <dbferreira@stone.com.br>
2017-09-25 20:57:40 -0300

    Fix broken tests

--- tests/unit/test_job_model.py
+++ tests/unit/test_job_model.py
@@ -8,16 +8,15 @@ from bs4 import BeautifulSoup
 def test_should_extract_resources_from_job_xml():
     job = Job('examplejob')
     test_xml = open('tests/files/complete_job.xml', encoding='utf-8')
-    print(test_xml.read())
-    print(test_xml)
-    #job_xml = BeautifulSoup(test_xml, "xml")
-    #job.add_resources_from_xml(job_xml)
+
+    job_xml = BeautifulSoup(test_xml, "xml")
+    job.add_resources_from_xml(job_xml)
 
     resource1 = Resource('.net builder')
     resource2 = Resource('windows')
     expected_resources = [resource1, resource2]
 
-    assert (len(job.get_resources()) == len(expected_resources) +1) and (expected_resources == job.get_resources())
+    assert (len(job.get_resources()) == len(expected_resources)) and (expected_resources == job.get_resources())
 
 def test_should_extract_resources_from_job_xml_without_resources():
     job = Job('examplejob')
